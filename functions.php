e28113d86cd67a0a866f16601beb4b7562d79dce
JessicaQC <79950970+JessicaQC@users.noreply.github.com>
2021-04-16 10:11:39 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,5 +8,5 @@ wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap
 }
  
 add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' );
-
+add_theme_support( 'post-thumbnails'); 
 ?>
